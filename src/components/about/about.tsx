35edaa3a82ab2d9e45b9f7af0fbad34506081200
서환주 <>
2025-07-20 19:00:16 +0900

    about 추가

---
+++ src/components/about/about.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import styles from "./about.module.scss";
+
+export default function About(): JSX.Element {
+  const skills = [
+    { src: "/ico-html5.png", alt: "HTML5", label: "HTML5" },
+    { src: "/ico-css3.png", alt: "CSS3", label: "CSS3" },
+    { src: "/ico-js.png", alt: "JavaScript", label: "JavaScript" },
+    { src: "/ico-jquery.png", alt: "jQuery", label: "jQuery" },
+    { src: "/ico-sass.png", alt: "Sass", label: "Sass" },
+    { src: "/ico-a11y.png", alt: "웹표준/웹접근성", label: "웹표준/웹접근성" },
+    { src: "/ico-vue.png", alt: "Vue.js", label: "Vue.js" },
+    { src: "/ico-react.png", alt: "React", label: "React" },
+    { src: "/ico-next.png", alt: "Next.js", label: "Next.js" },
+    { src: "/ico-git.png", alt: "Git", label: "Git" },
+    { src: "/ico-ps.png", alt: "Photoshop", label: "Photoshop" },
+  ];
+
+  return (
+    <>
+      <h2>About Me</h2>
+      <div className={styles.aboutMe}>
+        <Image
+          src="/me.png"
+          alt="의자에 앉아서 웃고있는 서환주 사진"
+          width={200}
+          height={200}
+        />
+        <p>
+          안녕하세요.
+          <br />
+          웹 퍼블리셔로 7년간 일한 뒤 프론트엔드 개발로 커리어를 확장해 2년째
+          경력을 이어가고 있는 서환주입니다.
+          <br />
+          제 포트폴리오 사이트에 방문해 주셔서 감사합니다 🙂
+          <br />
+          <br />
+          함께 고민하고 협업하며 하나씩 만들어가는 과정을 즐깁니다.
+          <br />
+          완성된 결과물에서 느끼는 뿌듯함이 저를 더욱 성장하게 만듭니다.
+          <br />
+          좋아하는 일을 꾸준히 하다 보니 자연스럽게 실력이 쌓여왔고,
+          <br />
+          주어진 역할에서 항상 최선을 다하는 성실한 개발자입니다.
+        </p>
+      </div>
+      <h2>Skills</h2>
+      <div className={styles.skills}>
+        <ul>
+          {skills.map((item) => (
+            <li key={item.label}>
+              <Image src={item.src} alt={item.alt} width={58} height={58} />
+              <span>{item.label}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
